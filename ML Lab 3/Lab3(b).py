cdb425d797a7d70af9f5a7701f12e1c7a75aa1f8
Gabriel Boribor <98925504+GabrielBoribor@users.noreply.github.com>
2024-02-19 09:25:24 +0800

    Add files via upload

---
+++ ML Lab 3/Lab3(b).py
@@ -0,0 +1,14 @@
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('CJ.png')
+GBlur = cv.GaussianBlur(img,(55,5),0)
+
+
+
+plt.subplot(121), plt.imshow(img),plt.title('Original')
+plt.xticks([]), plt.yticks([])
+
+plt.subplot(122), plt.imshow(GBlur),plt.title('Gaussian Blur')
+plt.xticks([]), plt.yticks([])
+plt.show()
